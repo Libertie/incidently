cc2a450ddc98782adcb1b3cef673a7797df53f9f
Libertie <745156+Libertie@users.noreply.github.com>
2020-03-15 13:20:22 -0400

    Show Person template updates

--- resources/views/people/show.blade.php
+++ resources/views/people/show.blade.php
@@ -2,48 +2,35 @@
 
 @section('content')
 
-<h1>{{ $person->name }}</h1>
+<h1>{{ $person->name ?: 'Name Unknown' }}</h1>
+<?= $person->nickname ? "<p class=\"h2 text-muted\">{$person->nickname}</p>" : '' ?> 
 
-<div class="card">
-    <div class="card-header">
-        Description
-    </div>
-    <div class="card-body">
-        <p>
-            Hair Color:
-            {{ $person->hair_color }}
-        </p>
-        <p>
-            Hair Length:
-            {{ $person->hair_length }}
-        </p>
-        <p>
-            Facial Hair:
-            {{ $person->facial_hair }}
-        </p>
-        <p>
-            Height:
-            {{ $person->height }}
-        </p>
-        <p>
-            Skin:
-            {{ $person->skin }}
-        </p>
-        <p>
-            Gender:
-            {{ $person->gender }}
-        </p>
-        <p>
-            Voice:
-            {{ $person->voice }}
-        </p>
-        <p>
-            Age:
-            {{ $person->age }}
-        </p>
-        <p>
-            {{ $person->description }}
-        </p>
+<p class="lead">
+    {{ $person->description }}
+</p>
+
+
+<div class="row">
+    <div class="col-6">
+        <div class="card">
+            <div class="card-header">
+                Description
+            </div>
+            <div class="card-body">
+                <dl class="row">
+<?php
+foreach ([
+    'hair_color', 'hair_length', 'facial_hair', 'height', 'Skin complexion' => 'skin', 'gender', 'voice', 'age'
+] as $label => $property) :
+?>
+                    <dt class="col-6">{{ is_string($label) ? $label : ucwords(str_replace('_', ' ', $property)) }}:</dt>
+                    <dd class="col-6">{{ ucwords($person->$property) ?: 'Unknown' }}</dd>
+<?php
+endforeach;
+?>
+                </dl>
+            </div>
+        </div>
     </div>
 </div>
 
